ff5582d2ad286d43fa336506ec93f195ca805633
dwqs <461147874@qq.com>
2017-04-23 18:26:08 +0800

    api

---
+++ client/network/api.js
@@ -0,0 +1,11 @@
+
+/**
+ * Created by pomy on 2017/4/12.
+ * api 封装
+ */
+
+import network from './index';
+
+export default {
+
+};
---
+++ client/network/index.js
@@ -0,0 +1,40 @@
+/**
+ * Created by pomy on 2017/4/12.
+ * 网络请求封装
+ */
+
+export default {
+    get (obj) {
+        return Promise.resolve(
+            $.ajax({
+                method: 'GET',
+                url: `${obj.url}`,
+                data: obj.data
+            })
+        );
+    },
+
+    post (obj) {
+        return Promise.resolve(
+            $.ajax({
+                url: `${obj.url}`,
+                method: 'POST',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify(obj.data)
+            })
+        );
+    },
+
+    delete (obj) {
+        return Promise.resolve(
+            $.ajax({
+                url: `${obj.url}`,
+                method: 'DELETE',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify(obj.data)
+            })
+        );
+    }
+};
